0525542a53fe5e54beb3682fbffd5a081595a3cc
Ahmad Shahzad <42749059+amd257@users.noreply.github.com>
2021-02-26 03:00:39 +0000

    predictions now shown on the webpage properly
    still need to work on the frontend and also add option of multiple models

--- COVIDWebApp/flaskApp.py
+++ COVIDWebApp/flaskApp.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, flash, request, redirect, url_for,render_template
+from flask import Flask, flash, request 
+from flask import redirect, url_for,render_template,jsonify
 from werkzeug.utils import secure_filename
 from keras.models import load_model
 from keras.preprocessing import image
@@ -11,10 +12,11 @@ import logging
 
 UPLOAD_FOLDER = 'static//images//'
 ALLOWED_EXTENSIONS = { 'png', 'jpg', 'jpeg'}
+SECRET_KEY = 'T\xee\x9d?\xf9\xcfMs\xd0\xf0\x01$\xc2\xd5\xd7XJ\xb9{\xfbd\xa6?_'
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
+app.secret_key = SECRET_KEY
 
 def allowed_filename(filename):
 
@@ -39,7 +41,7 @@ def model_loader():
 model = model_loader() #Calling model globally so that it can be used freely by other methods
 
 #function for preprocessing the image
-def preprocess_image(new_image):
+def image_preprocessing(new_image):
 
 	pp_img = image.img_to_array(new_image)
 	pp_img = pp_img / 255.0 #rescale the image by .1/255
@@ -49,23 +51,30 @@ def preprocess_image(new_image):
 
 @app.route("/" , methods =["GET" , "POST"])
 def index():
-
 	return(render_template("index.html"))
 
 
 @app.route("/predict" , methods =["GET" , "POST"])
 def predict():
 
+	prediction =[]
 	if request.method == "POST":
-		
+		# check if the post request has the file part
 		if 'imagefile' not in request.files:
-			app.logger('No image file selected')
-			return redirect(request.url)
+			#app.logger.info('No image file selected')
+			flash('No image file selected')
+			return(redirect("index.html"))
+			#return redirect(url_for('index'))
+			#return redirect(request.url)
 	    
 		imagefile = request.files["imagefile"]
+		# if user does not select a file, browser can also submit an empty file without filename
 		if imagefile.filename == "":
-			app.logger('No image file selected')
-			return redirect(request.url)
+			#app.logger.info('No image file selected')
+			flash('No image file selected')
+			return(redirect("index.html"))
+			#return redirect(request.url)
+			#return redirect(url_for('index'))
 		
 		if imagefile and allowed_filename(imagefile.filename):
 			filename=secure_filename(imagefile.filename)
@@ -75,17 +84,28 @@ def predict():
 
 			new_image = image.load_img(img_path, target_size=(256, 256)) #loading image using the keras image preprocessing library
 			app.logger.info('keras preprocessing image library has loaded the image')
-			preprocessed_image = preprocess_image(new_image)
+			preprocessed_image = image_preprocessing(new_image)
 			prediction = model.predict(preprocessed_image).tolist()
 			app.logger.info('Prediction has been done: ')
-			app.logger.info(prediction)
-			
+			app.logger.info(prediction[0][0])
+			app.logger.info(prediction[0][1])
+			app.logger.info(prediction[0][2])
+			os.remove(img_path)
+			app.logger.info('Image removed as prediction has been made and it was no longer needed')
 	
-	return(render_template("index.html")) 
+
+	CovidPred = prediction[0][0]  
+	NormalPred = prediction[0][1] 
+	PneumoniaPred = prediction[0][2] 
+	
+	return render_template('predict.html', 
+							CovidPred= CovidPred, 
+							NormalPred = NormalPred, 
+							PneumoniaPred = PneumoniaPred)
 
 
-
-
+if __name__ == '__main__':
+	app.run(debug=True)
 
 
 '''@app.route("/predict", methods=['GET' , 'POST'])
@@ -107,5 +127,4 @@ def predict():
 			flash('image path is: ', imgPath)
 			
 '''
-if __name__ == '__main__':
-	app.run(debug=True)
+
