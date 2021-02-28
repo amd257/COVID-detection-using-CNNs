900a2d4fd81810e3b0c32f94165e5b37cc52f09c
Ahmad Shahzad <42749059+amd257@users.noreply.github.com>
2021-02-28 15:59:46 +0000

    Added the binary classifcation page and the homepage
    Backend finished. Might want to add the "the model predicts this is % covid, etc" line, but that's not necessary

--- COVIDWebApp/finalWebApp.py
+++ COVIDWebApp/finalWebApp.py
@@ -54,49 +54,111 @@ def image_preprocessing(new_image):
 
 	img = image.img_to_array(new_image)
 	img = img / 255.0 #rescale the image by .1/255
-	preprocessed_img = np.expand_dims(img, axis=0)# expand image dimensions to fit the model input dimensions
+	preprocessed_img = np.expand_dims(img, axis=0)# expands image dimensions to fit the model input dimensions
 	return preprocessed_img
 
 
 
 @app.route("/" , methods =["GET" , "POST"])
-def home():
-	return(render_template("threeLabelPrediction.html")) #change this to homepage.html if you set that up
+def homepage():
+	return(render_template("homepage.html"))
 
 
 #function for making the three label prediction between COVID, Normal and Pneumonia images
-@app.route("/threeLabelPrediction" , methods =["GET" , "POST"])
-def threeLabelPrediction():
-	prediction =[[-1,-1,-1]]
+@app.route("/binaryPrediction" , methods =["GET" , "POST"])
+def binaryPrediction():
+	
+	#Initialising the variables to be returned to the frontend using jinja2
+	CovidPred = 0
+	NonCovidPred = 0
 	img_path=""
-	if request.method == "POST":
 
+	if request.method == "POST":
 		#remove previous images in the upload folder that are not required
 		clear_images_directory()
 		# check if the post request has the file part
 		if 'imagefile' not in request.files:
 			app.logger.info('No image file selected')
 			flash('Please upload image file before clicking the predict button')
-			#return redirect(url_for("threeLabelPrediction"))
-			return render_template('threeLabelPrediction.html')
+			return render_template('binaryPrediction.html')
 	    
 		imagefile = request.files["imagefile"]
+
 		# if user does not select a file, browser can also submit an empty file without filename
 		if imagefile.filename == "":
 			app.logger.info('No image file selected')
 			flash('No image file selected. Please select an image file before clicking predict')
-			#return redirect(url_for("threeLabelPrediction"))
-			return render_template('threeLabelPrediction.html')
+			return render_template('binaryPrediction.html')
 		
 		if imagefile and allowed_filename(imagefile.filename):
 			filename=secure_filename(imagefile.filename)
-			
-			#app.logger.info("Previous images have been cleared from the directory")
 			img_path= os.path.join(app.config['UPLOAD_FOLDER'], filename)
 			imagefile.save(img_path)
 			app.logger.info(img_path)
 
-			model = threelabel_model_loader() #Calling the three label model
+			model = binary_model_loader() #Calling the binary classification Deep Learning model
+			app.logger.info('Binary Classification Model has been loaded successfully')
+
+			new_image = image.load_img(img_path, target_size=(256, 256), color_mode="grayscale") #loading image using the keras image preprocessing library
+			app.logger.info('Keras preprocessing image library has loaded the image')
+			preprocessed_image = image_preprocessing(new_image)
+			prediction = model.predict(preprocessed_image).tolist()
+			app.logger.info('Prediction has been done:-')
+			#if prediction[0][0] >=0.5:
+				#app.logger.info('The model is {:.2%} percent confident that this is a Non-COVID case ', prediction[0][0]) #As Non-COVID is label 1 in the model
+			#else:
+				#app.logger.info('The model is {:.2%} percent confident that this is a COVID case ', 1-prediction[0][0]) #As COVID is label 0 in the model
+
+			NonCovidPred = round(((prediction[0][0])*100), 2 )
+			CovidPred = round(((1 - prediction[0][0])*100), 2 ) 
+	
+
+		else:
+			flash('The file selected was not in the correct format. Please upload X-ray image in .png, .jpg or .jpeg format')
+
+	
+
+	return render_template('binaryPrediction.html', 
+							CovidPred= CovidPred, 
+							NonCovidPred = NonCovidPred,
+							imgsrc = img_path)
+
+
+
+
+
+
+
+#function for making the three label prediction between COVID, Normal and Pneumonia images
+@app.route("/threeLabelPrediction" , methods =["GET" , "POST"])
+def threeLabelPrediction():
+	prediction =[[0,0,0]]
+	img_path=""
+
+	if request.method == "POST":
+		#remove previous images in the upload folder that are not required
+		clear_images_directory()
+		# check if the post request has the file part
+		if 'imagefile' not in request.files:
+			app.logger.info('No image file selected')
+			flash('Please upload image file before clicking the predict button')
+			return render_template('threeLabelPrediction.html')
+	    
+		imagefile = request.files["imagefile"]
+
+		# if user does not select a file, browser can also submit an empty file without filename
+		if imagefile.filename == "":
+			app.logger.info('No image file selected')
+			flash('No image file selected. Please select an image file before clicking predict')
+			return render_template('threeLabelPrediction.html')
+		
+		if imagefile and allowed_filename(imagefile.filename):
+			filename=secure_filename(imagefile.filename)
+			img_path= os.path.join(app.config['UPLOAD_FOLDER'], filename)
+			imagefile.save(img_path)
+			app.logger.info(img_path)
+
+			model = threelabel_model_loader() #Calling the three label Deep Learning model
 			app.logger.info('Three Label Model has been loaded successfully')
 
 			new_image = image.load_img(img_path, target_size=(256, 256), color_mode="grayscale") #loading image using the keras image preprocessing library
@@ -114,8 +176,6 @@ def threeLabelPrediction():
 	CovidPred = round(((prediction[0][0])*100), 2 ) 
 	NormalPred = round(((prediction[0][1])*100), 2 )
 	PneumoniaPred = round(((prediction[0][2])*100), 2 )
-
-
 
 	return render_template('threeLabelPrediction.html', 
 							CovidPred= CovidPred, 
